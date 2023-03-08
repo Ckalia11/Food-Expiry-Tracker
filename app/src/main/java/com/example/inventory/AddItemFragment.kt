f1cc31b27578215c3a76fcae6bfe491da11c28d7
epheitea <ephei@outlook.com>
2023-03-09 00:38:33 +0700

    Added conditional rendering of imageView (mocked upload image feature)

--- app/src/main/java/com/example/inventory/AddItemFragment.kt
+++ app/src/main/java/com/example/inventory/AddItemFragment.kt
@@ -85,6 +85,7 @@ class AddItemFragment : Fragment() {
         }
     }
 
+
     /**
      * Inserts the new Item into database and navigates up to list fragment.
      */
@@ -126,14 +127,21 @@ class AddItemFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val id = navigationArgs.itemId
+        // Protocol for editing an existing item
         if (id > 0) {
             viewModel.retrieveItem(id).observe(this.viewLifecycleOwner) { selectedItem ->
                 item = selectedItem
                 bind(item)
             }
+            binding.imageView.visibility = View.VISIBLE
+
+        // Protocol for adding a new item
         } else {
             binding.saveAction.setOnClickListener {
                 addNewItem()
+            }
+            binding.uploadPhoto.setOnClickListener{
+                binding.imageView.visibility = View.VISIBLE
             }
         }
     }
