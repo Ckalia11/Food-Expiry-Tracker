150b4937fe6a565e92980ac9c778f1f0ee92e587
tayyaab t <tayyaabtanveer@gmail.com>
2023-04-05 02:57:52 -0400

    fix bug to auto-select all if all options selected

--- app/src/main/java/com/example/inventory/MultiSelectSpinnerAdapter.kt
+++ app/src/main/java/com/example/inventory/MultiSelectSpinnerAdapter.kt
@@ -1,14 +1,16 @@
 package com.example.inventory
+
 import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import kotlinx.coroutines.selects.select
 
 class MultiSelectSpinnerAdapter(
     private val context: Context,
-    private val itemList: List<String>,
+    private val itemList: List<String>
 ) : BaseAdapter() {
 
     private val selectedItems = mutableListOf<String>()
@@ -36,7 +38,8 @@ class MultiSelectSpinnerAdapter(
                 notifyDataSetChanged()
             }
         } else {
-            val selectAllCheckbox = parent?.getChildAt(0)?.findViewById<CheckBox>(R.id.recipeDropdownCheckbox)
+            val selectAllCheckbox =
+                parent?.getChildAt(0)?.findViewById<CheckBox>(R.id.recipeDropdownCheckbox)
             textView.text = itemList[position]
             checkBox.isChecked = selectedItems.contains(itemList[position])
             checkBox.setOnCheckedChangeListener { _, isChecked ->
@@ -44,39 +47,38 @@ class MultiSelectSpinnerAdapter(
                     if (!selectedItems.contains(itemList[position])) {
                         selectedItems.add(itemList[position])
                     }
+                    if (selectAllCheckbox?.isChecked == false && selectedItems.size == itemList.size - 1) {
+                        selectAllCheckbox.isChecked = true
+                    }
                 } else {
                     selectedItems.remove(itemList[position])
-                }
-                // Uncheck "Select All" checkbox if any other checkbox is unchecked
-                // TODO: This is kinda buggy right now. Perhaps pass in a parameter to the onclicklistener
-                // To determine where the call is coming from. If it's coming from here then we simply
-                // Uncheck the "Select All" button but then we DON'T trigger the array.clear()
-                if (!isChecked && selectedItems.size >= itemList.size - 1) {
-                    selectAllCheckbox?.isChecked = false
+                    if (selectAllCheckbox?.isChecked == true) {
+                        selectAllCheckbox.isChecked = false
+                    }
                 }
             }
-            }
-
-            return view
         }
 
-        override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View {
-            return getView(position, convertView, parent)
-        }
-
-        override fun getItem(position: Int): Any {
-            return itemList[position]
-        }
-
-        override fun getItemId(position: Int): Long {
-            return position.toLong()
-        }
-
-        override fun getCount(): Int {
-            return itemList.size
-        }
-
-        fun getSelectedItems(): List<String> {
-            return selectedItems
-        }
+        return view
     }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        return getView(position, convertView, parent)
+    }
+
+    override fun getItem(position: Int): Any {
+        return itemList[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getCount(): Int {
+        return itemList.size
+    }
+
+    fun getSelectedItems(): List<String> {
+        return selectedItems
+    }
+}
